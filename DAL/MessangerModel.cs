4e638a4a14c0bb0d1ebbeb08c5b5bd020cf6eb59
Dima <dg646726@gmail.com>
2021-03-24 11:07:37 +0200

    Create Db Model

---
+++ DAL/MessangerModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace DAL
+{
+    public class MessangerModel : DbContext
+    {
+        // Your context has been configured to use a 'MessangerModel' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'DAL.MessangerModel' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'MessangerModel' 
+        // connection string in the application configuration file.
+        public MessangerModel()
+            : base("name=MessangerModel")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
