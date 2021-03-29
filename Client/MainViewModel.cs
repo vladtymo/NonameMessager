de8a591aada6f1d8f673712c00da6e05fad9dbb0
Dima <dg646726@gmail.com>
2021-03-29 15:21:27 +0300

    Create ViewModels

---
+++ Client/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class MainViewModel
+    {
+        private ClientViewModel currentClient;
+        private ClientViewModel clientForChange;
+
+        public MainViewModel()
+        {
+
+        }
+
+
+        public ClientViewModel CurrentClient => currentClient;
+        public ClientViewModel ClientForChange => clientForChange;
+    }
+}
---
+++ Client/ViewModels/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Client
+{
+    public class AccountViewModel : ViewModelBase
+    {
+        private string email;
+        private string phone;
+
+        public int Id { get; set; }
+
+        public string Email { get => email; set => SetProperty(ref email, value); }
+        public string Phone { get => phone; set => SetProperty(ref phone, value); }
+    }
+}
