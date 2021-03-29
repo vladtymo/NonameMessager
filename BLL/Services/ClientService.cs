5f4e72a6d97d974bf91291977a8a3304019d2c75
Dima <dg646726@gmail.com>
2021-03-29 17:27:00 +0300

    Add methods in client service

--- BLL/Services/ClientService.cs
+++ BLL/Services/ClientService.cs
@@ -12,7 +12,9 @@ namespace BLL
     public interface IClientService
     {
         IEnumerable<ClientDTO> GetAllClients();
-        void CreateNewClient(ClientDTO clientDTO);
+        ClientDTO CreateNewClient(ClientDTO clientDTO);
+        ClientDTO GetClient(AccountDTO accountDTO);
+        bool SetProperties(ClientDTO clientDTO);
     }
     public class ClientService : IClientService
     {
@@ -43,10 +45,62 @@ namespace BLL
             var result = repositories.ClientRepos.Get(includeProperties: $"{nameof(Client.Account)}");
             return mapper.Map<IEnumerable<ClientDTO>>(result);
         }
-        public void CreateNewClient(ClientDTO clientDTO)
+        private Client GetClientByAccount(AccountDTO accountDTO)
         {
-            repositories.ClientRepos.Insert(mapper.Map<Client>(clientDTO));
-            repositories.Save();
+            var client = repositories.ClientRepos.Get().Where(c => c.Account.Email == accountDTO.Email || c.Account.Phone == accountDTO.Phone && c.Account.Password == ComputeSha256Hash(accountDTO.Password)).FirstOrDefault();
+            if (client != null)
+                return client;
+            else
+                return null;
+        }
+        private int IsExistClient(ClientDTO clientDTO)
+        {
+            var client = repositories.ClientRepos.Get().Where(c => c.UniqueName == clientDTO.UniqueName || c.Account.Email == clientDTO.Account.Email || c.Account.Phone == clientDTO.Account.Phone).FirstOrDefault();
+            if (client == null)
+                return -1;
+            else
+                return client.Id;
+        }
+        public ClientDTO CreateNewClient(ClientDTO clientDTO)
+        {
+            var id = IsExistClient(clientDTO);
+            if (id == -1)
+            {
+                repositories.ClientRepos.Insert(mapper.Map<Client>(clientDTO));
+                repositories.Save();
+                repositories.ClientRepos.Get().LastOrDefault().AccountId = repositories.AccountRepos.Get().LastOrDefault().ClientId;
+                repositories.Save();
+                return mapper.Map<ClientDTO>(repositories.ClientRepos.Get().LastOrDefault());
+            }
+            else
+                return null;
+        }
+        public ClientDTO GetClient(AccountDTO accountDTO)
+        {
+            var client = GetClientByAccount(accountDTO);
+            if (client != null)
+            {
+                return mapper.Map<ClientDTO>(client);
+            }
+            else
+                return null;
+        }
+        public bool SetProperties(ClientDTO clientDTO)
+        {
+            var id = IsExistClient(clientDTO);
+            if (id != -1)
+            {
+                var client = repositories.ClientRepos.Get().Where(c => c.Id == id).FirstOrDefault();
+                client.Name = clientDTO.Name;
+                client.UniqueName = clientDTO.UniqueName;
+                client.Account.Email = clientDTO.Account.Email;
+                client.Account.Phone = clientDTO.Account.Phone;
+                repositories.ClientRepos.Update(client);
+                repositories.Save();
+                return true;
+            }
+            else
+                return false;
         }
         static string ComputeSha256Hash(string rawData)
         {
--- Client/ViewModels/AccountViewModel.cs
+++ Client/ViewModels/AccountViewModel.cs
@@ -4,10 +4,11 @@
     {
         private string email;
         private string phone;
-
+        private string password;
         public int Id { get; set; }
 
         public string Email { get => email; set => SetProperty(ref email, value); }
         public string Phone { get => phone; set => SetProperty(ref phone, value); }
+        public string Password { get => password; set => SetProperty(ref password, value); }
     }
 }
