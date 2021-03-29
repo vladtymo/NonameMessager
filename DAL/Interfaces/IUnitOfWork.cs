8def0766b997a23db518a884a191e59559e624f3
Dima <dg646726@gmail.com>
2021-03-29 13:02:03 +0300

    Implement GenericRepository and UnitOfWork patterns

---
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Account> AccountRepos { get; }
+        IRepository<Chat> ChatRepos { get; }
+        IRepository<ChatMember> ChatMemberRepos { get; }
+        IRepository<Client> ClientRepos { get; }
+        IRepository<Contact> ContactRepos { get; }
+        IRepository<File> FileRepos { get; }
+        IRepository<Message> MessageRepos { get; }
+        void Save();
+    }
+}
