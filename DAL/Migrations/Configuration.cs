d1097c3367ece9849c1d52475925d0bcbae5c163
dg646726 <61685741+dg646726@users.noreply.github.com>
2021-04-02 12:49:09 +0300

    Add Migrations and Update ChatEntity, ChatDTO

---
+++ DAL/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.MessangerModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "DAL.MessangerModel";
+        }
+
+        protected override void Seed(DAL.MessangerModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
