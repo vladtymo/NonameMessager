506269d2cfa049fb9b9a1ab6422da8f83856a615
dg646726 <61685741+dg646726@users.noreply.github.com>
2021-04-05 13:38:46 +0300

    Add Data Transfer Objects

---
+++ WcfService/DataTransferObject/AccountDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService
+{
+    [DataContract]
+    public class AccountDTO
+    {
+        [DataMember]
+        public int ClientId { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
---
+++ WcfService/DataTransferObject/ChatDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfService
+{
+    [DataContract]
+    public class ChatDTO
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string PhotoPath { get; set; }
+        [DataMember]
+        public string UniqueName { get; set; }
+        [DataMember]
+        public bool IsPrivate { get; set; }
+        [DataMember]
+        public int MaxUsers { get; set; }
+        [DataMember]
+        public bool IsPM { get; set; }
+
+    }
+}
