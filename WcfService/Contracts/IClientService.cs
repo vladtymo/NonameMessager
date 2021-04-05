7509ec62c2f76ad695a51e9dba06e58b193ef8c9
dg646726 <61685741+dg646726@users.noreply.github.com>
2021-04-05 13:35:43 +0300

    Add Contracts

---
+++ WcfService/Contracts/IClientService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IClientService
+    {
+        [OperationContract]
+        IEnumerable<ClientDTO> GetAllClients();
+        [OperationContract]
+        ClientDTO CreateNewClient(ClientDTO clientDTO, string password);
+        [OperationContract]
+        ClientDTO GetClient(AccountDTO accountDTO, string password);
+        [OperationContract]
+        bool SetProperties(ClientDTO clientDTO);
+    }
+}
